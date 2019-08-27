3feab6acf535392b0a01d1397a7cd18374b20499
pramodkharade <kharade.pramod91@gmail.com>
2019-08-27 12:58:07 +0530

    added cors error

--- app.js
+++ app.js
@@ -5,4 +5,9 @@ const app = express();
 //app.use(bodyParser.urlencoded())// x-www-form-url
 app.use(bodyParser.json()); // application/json
 app.use('/feed',feedRouter);
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Request-Headers','Content-Type,Authorization');
+});
 app.listen(8080);
