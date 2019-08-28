3e38ee9af3e68cdcc4982d4b0b87a4f1a9ef6520
pramodkharade <kharade.pramod91@gmail.com>
2019-08-28 12:17:52 +0530

    serve the static image

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const feedRouter = require('./routes/feed');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');   
+const path = require('path');
 const app = express();
 const MONGOURI ='mongodb://127.0.0.1:27017/blogNodeAPI';
 const port = process.env.PORT || 8080;
 //app.use(bodyParser.urlencoded())// x-www-form-url
 app.use(bodyParser.json()); // application/json
+app.use('/images',express.static(path.join(__dirname,'images')));
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
