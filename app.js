84da4100e52f566527f8f14b994d57ec123e381b
pramodkharade <kharade.pramod91@gmail.com>
2019-08-28 09:44:19 +0530

    cors issue for external world while accessing the api

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const bodyParser = require('body-parser');
 const app = express();
 //app.use(bodyParser.urlencoded())// x-www-form-url
 app.use(bodyParser.json()); // application/json
-app.use('/feed',feedRouter);
 app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE');
-    res.setHeader('Access-Control-Request-Headers','Content-Type,Authorization');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
 });
+app.use('/feed',feedRouter);
 app.listen(8080);
