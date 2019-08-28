b2cd2340dfb0b2988045a0319061967e6959f8f7
pramodkharade <kharade.pramod91@gmail.com>
2019-08-28 11:24:46 +0530

    Server side validation with routes and controller

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,4 @@
+const {validationResult} = require('express-validator');
 exports.getPosts= (req,res,next)=>{
     res.status(200).json({'posts':[{ 
         _id:'1',
@@ -11,6 +12,13 @@ exports.getPosts= (req,res,next)=>{
        }]});
 };
 exports.createPost = (req,res,next)=>{
+    const error = validationResult(req);
+    if(!error.isEmpty()){
+        return res.status(422).json({
+            message:'Validation failed, entered data is incorrect.',
+            error: error.array()
+        });
+    }
     const title = req.body.title;
     const content = req.body.content;
     /****Create db connection****/
--- routes/feed.js
+++ routes/feed.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const {body} = require('express-validator');
 const router = express.Router();
 const feedController = require('../controllers/feed');
 router.get('/posts',feedController.getPosts);
-router.post('/post',feedController.createPost);
+router.post('/post',[
+    body('title').trim().isLenght({min:5}).withMessage('Please enter valid title'),
+    body('content').trim().isLenght({min:5}).withMessage('Please enter valid content')
+],feedController.createPost);
 module.exports = router;
