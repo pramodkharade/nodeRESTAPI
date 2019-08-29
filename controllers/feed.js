670da89f6fad3c5bd9c37b508ec84844996afcf0
pramodkharade <kharade.pramod91@gmail.com>
2019-08-29 16:05:56 +0530

    added check to edit and delete post for owner of the post

--- controllers/feed.js
+++ controllers/feed.js
@@ -125,6 +125,11 @@ exports.updatePost = (req, res, next) => {
                 error.statusCode = 404;
                 throw error;
             }
+            if(post.creator.toString() !== req.userId){
+                const error = new Error('Not authorized');
+                error.statusCode = 403;
+                throw error;
+            }
             if (imageUrl !== post.imageUrl) {
                 clearImage(post.imageUrl);
             }
@@ -160,6 +165,11 @@ exports.deletePost = (req, res, next) => {
             if (!post) {
                 const error = new Error('Could not found post');
                 error.statusCode = 404;
+                throw error;
+            }
+            if(post.creator.toString() !== req.userId){
+                const error = new Error('Not authorized');
+                error.statusCode = 403;
                 throw error;
             }
             clearImage(post.imageUrl);
