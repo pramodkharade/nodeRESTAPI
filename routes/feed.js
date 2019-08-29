051c1ecafd4dbb4d8cd63f1c4b54f979b9de62f0
pramodkharade <kharade.pramod91@gmail.com>
2019-08-29 14:47:18 +0530

    added protection to routes using token

--- routes/feed.js
+++ routes/feed.js
@@ -3,16 +3,19 @@ const { body } = require('express-validator');
 const router = express.Router();
 const feedController = require('../controllers/feed');
 const isAuth = require('../middleware/is-auth');
-router.get('/posts',isAuth, feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
-router.post('/post', [
-    body('title').trim().isLength({ min: 5 }).withMessage('Please enter valid title'),
-    body('content').trim().isLength({ min: 5 }).withMessage('Please enter valid content'),
-], feedController.createPost);
-router.get('/post/:postId', feedController.getPost);
-router.put('/updatepost/:postId', [
+router.post('/post', isAuth,
+    [
+        body('title').trim().isLength({ min: 5 }).withMessage('Please enter valid title'),
+        body('content').trim().isLength({ min: 5 }).withMessage('Please enter valid content'),
+    ],
+    feedController.createPost
+);
+router.get('/post/:postId', isAuth, feedController.getPost);
+router.put('/updatepost/:postId', isAuth, [
     body('title').trim().isLength({ min: 5 }).withMessage('Please enter valid title'),
     body('content').trim().isLength({ min: 5 }).withMessage('Please enter valid content'),
 ], feedController.updatePost);
-router.delete('/post/:postId',feedController.deletePost);
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 module.exports = router;
