f69ac87745ac0333eca7f53fc587d7a90b4324d1
pramodkharade <kharade.pramod91@gmail.com>
2019-08-29 12:09:41 +0530

    user singup and saved

--- routes/auth.js
+++ routes/auth.js
@@ -5,14 +5,14 @@ const userController = require('../controllers/auth');
 const User = require('../models/user');
 router.put('/signup',
     [
-        body('email').isEmail().withMessage('Please enter valid email').custom((value,{req})=>{
-            return User.findOne({email:value}).then(userDoc=>{
-                if(userDoc){
+        body('email').isEmail().withMessage('Please enter valid email').custom((value, { req }) => {
+            return User.findOne({ email: value }).then(userDoc => {
+                if (userDoc) {
                     return Promise.reject('E-mail address already exists');
                 }
             })
         }).normalizeEmail(),
-        body('password').trim().isLength({min:5}),
+        body('password').trim().isLength({ min: 5 }),
         body('name').trim().not().isEmpty()
     ],
     userController.singup);
