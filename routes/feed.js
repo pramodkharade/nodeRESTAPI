d221b324a79af437402d18905943d6e6ba0a32b3
pramodkharade <kharade.pramod91@gmail.com>
2019-08-29 14:37:42 +0530

    middleware to validate jwttoken in every request

--- routes/feed.js
+++ routes/feed.js
@@ -2,7 +2,8 @@ const express = require('express');
 const { body } = require('express-validator');
 const router = express.Router();
 const feedController = require('../controllers/feed');
-router.get('/posts', feedController.getPosts);
+const isAuth = require('../middleware/is-auth');
+router.get('/posts',isAuth, feedController.getPosts);
 
 router.post('/post', [
     body('title').trim().isLength({ min: 5 }).withMessage('Please enter valid title'),
