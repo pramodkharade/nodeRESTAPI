f52331234c99d8f104085daa22a5fe5647076078
pramodkharade <kharade.pramod91@gmail.com>
2019-08-29 15:52:23 +0530

    set relation between post and user while create post or edit

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { validationResult } = require('express-validator');
 const Post = require('../models/post');
+const User = require('../models/user');
 exports.getPosts = (req, res, next) => {
     const currentPage = req.query.page || 1;
     const perPage = 2;
@@ -46,20 +47,28 @@ exports.createPost = (req, res, next) => {
     const title = req.body.title;
     const content = req.body.content;
     const imageUrl = req.file.path.replace("\\", "/");
+    let creator;
     /****Create db connection****/
     const post = new Post({
         title: title,
         content: content,
         imageUrl: imageUrl,
-        creator: {
-            name: 'Pramod Kharade'
-        }
+        creator: req.userId
     });
-    post.save().then(result => {
-        console.log(result);
+    post.save()
+    .then(result=>{
+        return User.findById(req.userId);   
+    })
+    .then(user => {
+        creator = user;
+        user.posts.push(post);
+        return user.save();
+       
+    }).then(result=>{
         res.status(201).json({
             message: "Post is created successfully.",
-            post: result
+            post: post,
+            creator:{_id:creator._id,name:creator.name}
         })
     })
         .catch(error => {
